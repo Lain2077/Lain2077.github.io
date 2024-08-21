5fc5a877c4823fb63921710a0a945213aacf5ecd
Mina@Navi <betrayedgirl@protonmail.com>
2024-08-21 14:39:39 -0400

    Devin

---
+++ devbranch/js/functions.js
@@ -0,0 +1,22 @@
+function filterItems(category) {
+    // Remove 'active' class from all filter links
+    const filterLinks = document.querySelectorAll('.nav-filter a');
+    filterLinks.forEach(link => link.classList.remove('active'));
+    
+    // Add 'active' class to the clicked filter link
+    const activeLink = Array.from(filterLinks).find(link => link.textContent.trim().toLowerCase() === category.toLowerCase());
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+
+    // Show or hide items based on the selected category
+    const items = document.querySelectorAll('.tui-window');
+    items.forEach(item => {
+        const itemCategories = item.getAttribute('data-category').split(' ');
+        if (itemCategories.includes(category) || category === 'all') {
+            item.classList.add('show');
+        } else {
+            item.classList.remove('show');
+        }
+    });
+}
---
+++ devbranch/notes.md
@@ -0,0 +1,11 @@
+# NOTES
+Thumbnail size: 583 x 560
+## Projects to includee
+
+Etheralis
+LE CULTE - Deluge, Artifices, Autre?
+
+
+##Experince
+
+Femmes en tech
